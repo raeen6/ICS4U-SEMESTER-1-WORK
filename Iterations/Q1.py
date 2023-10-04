a2ba960a47b77ca268d0c2219bd841925c36ecd0
raeen6 <raeen6@gmail.com>
2023-10-04 13:19:39 -0400

    factorial

--- Iterations/Q1.py
+++ Iterations/Q1.py
@@ -7,11 +7,11 @@ while num != 0:
     total_product *= num
     num -= 1
 
-print(f"Factorial of {num2} is {total_product}")
+print(f"Factorial of {num2} is {total_product}.")
 
 # For Loop Calculator
 total_product2 = 1
 
 for i in range(num2, 0, -1):
     total_product2 *= i
-print(f"Factorial of {num2} is {total_product2}")
+print(f"Factorial of {num2} is {total_product2}.")
