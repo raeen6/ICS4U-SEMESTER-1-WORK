234f16f62ed8ddf89d1092771f8be2001045d67f
raeen6 <raeen6@gmail.com>
2023-10-04 13:35:53 -0400

    Is this number a prime number??

---
+++ Iterations/Q3.py
@@ -0,0 +1,13 @@
+num = int(input("Please enter an integer greater than 1: "))
+
+prime = True
+
+for i in range (2, num):
+    if num % i == 0:
+        prime = False
+        break
+
+if prime:
+    print(f"The number {num} is a prime number")
+else:
+    print(f"The number {num} is not a prime number")
