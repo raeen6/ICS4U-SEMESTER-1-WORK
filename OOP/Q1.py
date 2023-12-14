4b5f76e2cebee60ceeb2180f1c78c0e5cb84cb50
raeen6 <raeen6@gmail.com>
2023-12-14 13:19:33 -0500

    Created Class Car

---
+++ OOP/Q1.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, brand, model, year, mileage):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.mileage = mileage
+
+    def display(self):
+         print(f"The car is: Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Mileage: {self.mileage}")
+
+TeslaY = Car("Tesla", "Y", 2020, 45.5)
+
+TeslaY.display()
